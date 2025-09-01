69e5c6571211a96f6b288f4d6ee75a8ed7826b88
Abdelilah <lakrimabdelilah@gmail.com>
2025-09-01 15:05:58 +0100

    psuh

--- src/App.tsx
+++ src/App.tsx
@@ -344,7 +344,7 @@ function App() {
                 />
                 <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-40 transition-all duration-300 flex items-center justify-center">
                   <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                    <p className="text-white font-semibold">Training Session</p>
+                    <p className="text-white font-semibold"></p>
                   </div>
                 </div>
               </div>
